36b6c63e7bcb50128cdd136752bb28c7f6355a42
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-02 15:13:23 +0600

    some changes Chef History

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chefHistory.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Chinese master cafe!')
@@ -11,6 +14,18 @@ app.get('/', (req, res) => {
 app.get('/chef', (req, res) => {
     res.send(chef);
 })
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const chefWithId = chef.find(chef => chef.id === id);
+    if (chefWithId) {
+      res.send(chefWithId);
+    } else {
+      res.status(404).send('Chef not found');
+    }
+  });
+  
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
