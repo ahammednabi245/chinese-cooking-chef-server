8cf03189ed0373dba13dbf698c9d8f2bad14f196
ahammednabi245 <kaziahammed245nabi@gmail.com>
2023-05-02 09:50:08 +0600

    chefHistory.json file

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const chef = require('./data/chefHistory.json');
+
 app.get('/', (req, res) => {
-  res.send('Chinese cafe!')
+  res.send('Chinese master cafe!')
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
 })
 
 app.listen(port, () => {
